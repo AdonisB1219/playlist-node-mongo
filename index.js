0657d90eb0858cea276df1061ab3d5ef5f548f0b
Adonis <adonisbecerra1219@gmail.com>
2023-10-19 21:38:58 -0600

    quitar mongoose driver

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import {MongoClient} from "mongodb";
-import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 const port = 3000;
-mongoose.connect("mongodb://127.0.0.1:27017/playlists");
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 
 async function start(){
