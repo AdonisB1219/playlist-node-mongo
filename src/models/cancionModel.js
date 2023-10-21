d4f1dce3a71866178c1f13b58f770366a3ce40be
Adonis <adonisbecerra1219@gmail.com>
2023-10-20 18:06:18 -0600

    Agregar validaciones a modelos

--- src/models/cancionModel.js
+++ src/models/cancionModel.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 
 export const cancionSchema = new mongoose.Schema({
-    nombre: String,
-    artista: String,
-    album: String,
-    rating: Number
+    nombre: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    artista: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    album: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    }
 });
 
 export const Cancion = mongoose.model("Cancion", cancionSchema);
--- src/models/generoModel.js
+++ src/models/generoModel.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 import {cancionSchema} from "./cancionModel.js";
 
 const generoSchema = new mongoose.Schema({
-    genero: String,
+    genero: {
+        type: String,
+        unique: true,
+        minlength: 3
+    },
     canciones: [cancionSchema]
 });
 export const Genero = mongoose.model("Genero", generoSchema);
